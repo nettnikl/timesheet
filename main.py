ba511a1115c1b2218d1b9313a721976540a61e3b
Niklas J. Nett <niklas.j.nett@disroot.org>
2022-02-10 23:23:29 +0100

    Use dates

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from openpyxl import Workbook, load_workbook, drawing
+import datetime
 import PIL.Image
 
 workbook = load_workbook("template.xlsx")
 worksheet = workbook.active
 
+offset = 9
+first_day = datetime.date.today().replace(day=1)
 for row in range(24):
-    worksheet.cell(row=row+9, column=1, value=f"NEW VALUE {row}")
-    worksheet.cell(row=row+9, column=3, value=f"10:00:00")
-    worksheet.cell(row=row+9, column=4, value=f"14:30:00")
-    worksheet.cell(row=row+9, column=5, value=f"00:30:00")
+    d = first_day + datetime.timedelta(days=row)
+    worksheet.cell(row=row+offset, column=1, value=d.strftime("%a, %d.%m.%Y"))
+    worksheet.cell(row=row+offset, column=3, value=f"10:00:00")
+    worksheet.cell(row=row+offset, column=4, value=f"14:30:00")
+    worksheet.cell(row=row+offset, column=5, value=f"00:30:00")
 
 fp = open("sign.png", "rb")
 img = PIL.Image.open(fp)
